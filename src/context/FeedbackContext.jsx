7ae63412d21999d3a882fc7b79aace638af6f51c
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-02-01 21:06:17 -0500

    fetch data from JSON server backend

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,31 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This item is feedback item 2',
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: 'This item is feedback item 3',
-      rating: 7,
-    },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      'http://loaclhost:5000/feedback?_sort=id&_order=desc',
+    );
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   // Add Feedback
   const addFeedback = (newFeedback) => {
@@ -59,6 +58,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback: feedback,
         feedbackEdit: feedbackEdit, //piece of state that holds the item and boolean
+        isLoading: isLoading,
         deleteFeedback: deleteFeedback,
         addFeedback: addFeedback,
         editFeedback: editFeedback, // function when we click on the edit icon
